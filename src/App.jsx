fdc4ec8c5b8c0d9d343b33d78b4d19f8ab815b57
sebas Barrera <sebarreracaucali@gmail.com>
2025-10-19 18:49:45 -0500

    integracon de pantalla de carga

--- src/App.jsx
+++ src/App.jsx
@@ -8,22 +8,34 @@ const friendsData = [
   {  commonFriends: "13 amigos en común" },
   {  commonFriends: "28 amigos en común" }
 ];
-
 export function App() {
   const [users, setUsers]=useState([]);
+  const [isLoading, setLoading]=useState(true);
+  const [error, setError]=useState(null); 
   useEffect(()=>{
     const getUsers = async () => {
       try {
         const res = await fetch(`https://api.escuelajs.co/api/v1/users`);
+        if (!res.ok){
+          throw new Error(`Error de network`);
+        }
         const data = await res.json();
         setUsers(data.slice(0,friendsData.length));
       } catch (error) {
+        setError(error);
         console.error(`Error en users fetch`);
+      }finally{
+        setLoading(false);
       }
-    }
+    };
     getUsers();
   },[]);
 
+  if (isLoading) {
+    return <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/2023_Facebook_icon.svg/800px-2023_Facebook_icon.svg.png" 
+    alt="logoLoading" 
+    />;
+  }
   return(
     <div className='app-container'>
       {users.map((user, index)=>(
--- src/FacebookCard.jsx
+++ src/FacebookCard.jsx
@@ -1,5 +1,4 @@
-import { useState } from "react";
-import { useEffect } from "react";
+import { useState,useEffect } from "react";
 export function FacebookFollowCard({ userName, commonFriends, initialIsFriend }) {
   const [isFriend, setIsFriend] = useState(initialIsFriend);
   const text = isFriend? "Ahora son amigos":"confirmar"
